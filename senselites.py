1fea7cda85e3817d994866ba443f70a12e5d7889
Andrew Fray <andrew.fray@gmail.com>
2020-03-15 14:55:22 +0000

    colors are functors that take coords
    which will let us do fades

--- senselites.py
+++ senselites.py
@@ -3,25 +3,30 @@ from random import randrange, choice, shuffle, random
 import time
 from mote import Mote
 
-def color_constant(color):
+class ColorConstant:
     """holds this colour"""
-    while True:
-        yield color
+    constant_col = [0,0,0]
+    def __init__(self, constant_col):
+        self.constant_col = constant_col
 
-def color_random():
+    def get(self, coord):
+        return self.constant_col
+
+class ColorRandom:
     """avoids repetition"""
     prev_pattern = -1
     values = [127, 200, 255]
     patterns = [[1,1,0], [1,0,1], [0,1,1], [0,0,1], [0,1,0], [1,0,0]]
-    while True:
+
+    def get(self, coord):
         # don't want colours too similar, and don't want black:
-        pattern_index = randrange(len(patterns))
-        if pattern_index == prev_pattern:
-            continue
-        pattern = patterns[pattern_index]
-        c = [pattern[0]*choice(values), pattern[1]*choice(values), pattern[2]*choice(values)]
-        yield c
-        prev_pattern = pattern_index
+        pattern_index = randrange(len(self.patterns))
+        if pattern_index == self.prev_pattern:
+            return self.get(coord)
+        self.prev_pattern = pattern_index
+        pattern = self.patterns[pattern_index]
+        c = [pattern[0]*choice(self.values), pattern[1]*choice(self.values), pattern[2]*choice(self.values)]
+        return c
 
 def pattern_race():
     fills = [0,0,0,0]
@@ -65,24 +70,23 @@ def run():
     mote.configure_channel(3, 16, False)
     mote.configure_channel(4, 16, False)
 
-    rand_cols = color_random()
+    rand_cols = ColorRandom()
     
     patterns = [pattern_floodfill, pattern_scatter, pattern_race]
-    colors = [color_random, lambda: color_constant(next(rand_cols))]
+    colors = [ColorRandom, lambda: ColorConstant(rand_cols.get(None))]
     
     while True:
         pattern = choice(patterns)
         color = choice(colors)()
 
         for update in pattern():
-            c = next(color)
+            c = color.get(update)
             mote.set_pixel(update[0], update[1], c[0], c[1], c[2])
             mote.show()
             time.sleep(0.05)
         if random() < 0.33:
             continue
         for update in pattern_reverse(pattern()):
-            c = next(color)
             mote.set_pixel(update[0], update[1], 0, 0, 0)
             mote.show()
             time.sleep(0.05)
