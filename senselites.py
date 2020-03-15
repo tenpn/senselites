53b4b63e330ecc8f3e86d15e582873f23910f4b6
Andrew Fray <andrew.fray@gmail.com>
2020-03-15 14:31:48 +0000

    pulling colour out of patterns
    and added race pattern

--- senselites.py
+++ senselites.py
@@ -1,5 +1,5 @@
 """runs a sensory garden"""
-from random import randrange, choice, shuffle
+from random import randrange, choice, shuffle, random
 import time
 from mote import Mote
 
@@ -23,23 +23,38 @@ def color_random():
         yield c
         prev_pattern = pattern_index
 
-def pattern_scatter(colour):
+def pattern_race():
+    fills = [0,0,0,0]
+    completed = 0;
+    while completed < 4:
+        # pick an incomplete channel
+        c = randrange(len(fills))
+        if fills[c] == 16:
+            continue
+        # add to fills, set the pixel
+        yield (c+1, fills[c])
+        fills[c] += 1
+        if fills[c] >= 16:
+            completed += 1
+        
+        
+def pattern_scatter():
     pixels = [(c,p) for c in range(1,5) for p in range(16)]
     shuffle(pixels)
     for p in pixels:
-        yield (p[0], p[1], next(colour))
+        yield (p[0], p[1])
         
-def pattern_floodfill(colour):
+def pattern_floodfill():
     """fills each channel in order"""
     for channel in range(1,5):
         for pixel in range(16):
-            yield (channel, pixel, next(colour))
+            yield (channel, pixel)
 
 def pattern_reverse(other_pattern):
     """sets each pixel black in reverse order of other_pattern"""
     other_reversed = reversed(list(other_pattern))
     for update in other_reversed:
-        yield (update[0], update[1], [0,0,0])
+        yield (update[0], update[1])
 
 def run():
     """let's goooo"""
@@ -52,22 +67,24 @@ def run():
 
     rand_cols = color_random()
     
-    patterns = [pattern_floodfill, pattern_scatter]
+    patterns = [pattern_floodfill, pattern_scatter, pattern_race]
     colors = [color_random, lambda: color_constant(next(rand_cols))]
     
     while True:
-        p = choice(patterns)
-        c = choice(colors)()
+        pattern = choice(patterns)
+        color = choice(colors)()
 
-        for update in p(c):
-            mote.set_pixel(update[0], update[1], update[2][0], update[2][1], update[2][2])
+        for update in pattern():
+            c = next(color)
+            mote.set_pixel(update[0], update[1], c[0], c[1], c[2])
             mote.show()
             time.sleep(0.05)
-        for update in pattern_reverse(p(c)):
-            mote.set_pixel(update[0], update[1], update[2][0], update[2][1], update[2][2])
+        if random() < 0.33:
+            continue
+        for update in pattern_reverse(pattern()):
+            c = next(color)
+            mote.set_pixel(update[0], update[1], 0, 0, 0)
             mote.show()
             time.sleep(0.05)
-        mote.clear()
-        mote.show()
 
 run()
